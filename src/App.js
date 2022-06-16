b12bc005027b42c32c26065d1836331d20adf659
Ian Loubser <ian@redbear.studio>
2022-06-16 17:32:09 +0200

    working console logs, working import map, context menu for files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useCallback, useRef, useState, useEffect, useLayoutEffect } from 'react'
-import Editor, { useMonaco } from "@monaco-editor/react";
-import { v4 as uuidv4 } from 'uuid';
+import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
+import Editor from "@monaco-editor/react";
+import { v4 as uuidv4 } from "uuid";
 
-import { Hook, Unhook, Console, Decode } from 'console-feed'
-import SplitPane from 'react-split-pane';
-import Pane from 'react-split-pane/lib/Pane';
+import { Console } from "console-feed";
+import SplitPane from "react-split-pane";
+import Pane from "react-split-pane/lib/Pane";
+import { ContextMenuTrigger, ContextMenu, MenuItem } from "react-contextmenu";
 
-import { Sidebar } from './components/Sidebar'
+import { Sidebar } from "./components/Sidebar";
 
 /*
 TODO:
@@ -23,7 +24,6 @@ TODO:
 -> remove all previous script tags
 
 */
-
 
 const injectLoggingControl = (doc) => {
   const script = `
@@ -48,144 +48,261 @@ const injectLoggingControl = (doc) => {
         window.parent.postMessage({ event: 'LOG', level, args: args }, '*');
       }
     }
-  `
+  `;
 
-  var logScript = doc.createElement('script')
-  logScript.type = 'module'
-  logScript.innerHTML = script
-  doc.head.appendChild(logScript)
-  return doc
+  var logScript = doc.createElement("script");
+  logScript.innerHTML = script;
+  doc.head.appendChild(logScript);
+  return doc;
+};
+
+/**
+ * Build an importmap, injecting all the dependencies as data URL's
+ * @param {*} html 
+ * @param {*} files 
+ * @returns 
+ * 
+<script type="importmap">
+{
+  "imports": {
+    "moment": "/node_modules/moment/src/moment.js",
+    "lodash": "/node_modules/lodash-es/lodash.js"
+  }
 }
+</script>
+ */
+const injectDependenciesImportMap = (html, files) => {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, "text/html");
+  var head = doc.getElementsByTagName("head")[0];
+  let importsJson = { imports: {} };
+  for (let file of Object.keys(files)) {
+    console.log("Injecting file", file);
+    const bb = new Blob([files[file]], { type: getFileMimeType(file) });
+    const dataUrl = URL.createObjectURL(bb);
+    importsJson.imports[file] = dataUrl;
+  }
+
+  const importMap = doc.createElement("script");
+  importMap.type = "importmap";
+  importMap.innerHTML = JSON.stringify(importsJson, null, 4);
+  head.appendChild(importMap);
+
+  doc = injectLoggingControl(doc);
+
+  return doc.documentElement.outerHTML;
+};
 
 const injectDependencies = (html, files) => {
   var parser = new DOMParser();
   var doc = parser.parseFromString(html, "text/html");
-  var scripts = doc.getElementsByTagName("script")
-  for (let i=0; i<scripts.length; i++) {
-    const srcPath = scripts[i].src.split("/")
+  doc = injectLoggingControl(doc);
+  var scripts = doc.getElementsByTagName("script");
+  for (let i = 0; i < scripts.length; i++) {
+    const srcPath = scripts[i].src.split("/");
     if (srcPath.length > 1) {
-      const src = srcPath[srcPath.length - 1]
-      let newScript = document.createElement('script')
-      newScript.innerHTML = files[src]
-      scripts[i].replaceWith(newScript)
+      const src = srcPath[srcPath.length - 1];
+      let newScript = document.createElement("script");
+      newScript.innerHTML = files[src];
+      scripts[i].replaceWith(newScript);
     }
   }
-  doc = injectLoggingControl(doc)
 
-  return doc.documentElement.outerHTML
-}
+  return doc.documentElement.outerHTML;
+};
 
 const defaultCached = {
   "urn:default": {
     name: "index.html",
-    value: "<html>\n"+"\t<body>\n\t\n\t"+"</"+"body>\n\n"+"\t<script>\n\t// write some code here"+"\n\t</"+"script>\n"+"</html>"
-  }
-}
+    value:
+      "<html>\n" +
+      "\t<body>\n\t\n\t" +
+      "</" +
+      "body>\n\n" +
+      "\t<script>\n\t// write some code here" +
+      "\n\t</" +
+      "script>\n" +
+      "</html>",
+  },
+};
 
 const Tab = (props) => {
-  const color = props.active ? 'text-white border-b-2' : 'hover:bg-gray-500 italic text-green-300'
-  return <button draggable={true} onClick={() => props.onToggle(props.value)} 
-    className={`text-xs font-semibold ${color} px-4 block focus:outline-none border-white divide-x divide-light-blue-400`}>
-    {props.children}
-  </button>
-}
+  const color = props.active
+    ? "text-white border-b-2"
+    : "hover:bg-gray-500 italic text-green-300";
+  return (
+    <>
+      <ContextMenuTrigger id={`file_${props.children}`}>
+        <button
+          draggable={true}
+          onClick={() => props.onToggle(props.value)}
+          className={`text-xs font-semibold ${color} px-4 block focus:outline-none border-white divide-x divide-light-blue-400 h-full`}
+        >
+          {props.children}
+        </button>
+      </ContextMenuTrigger>
+      <ContextMenu
+        hideOnLeave={true}
+        id={`file_${props.children}`}
+        className="bg-gray-800 text-gray-200 cursor-pointer py-2 w-40 shadow-md text-sm z-50 border-2 border-gray-600"
+      >
+        <MenuItem
+          className="px-5 py-1 hover:bg-gray-500"
+          onClick={() => console.log("delete")}
+        >
+          Delete
+        </MenuItem>
+        <MenuItem
+          className="px-5 py-1 hover:bg-gray-500"
+          onClick={() => console.log("rename")}
+        >
+          Rename
+        </MenuItem>
+      </ContextMenu>
+    </>
+  );
+};
 
 const NewFileTab = ({ onNewFile }) => {
-  const [editing, setEditing] = useState(false)
-  const [value, setValue] = useState('')
-  const inputRef = useRef()
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("");
+  const inputRef = useRef();
 
   const updateValue = (e) => {
-    setValue(e.target.value)
-  }
+    setValue(e.target.value);
+  };
 
   const onKeyPress = (e) => {
-    if (e.code === 'Enter') inputRef.current.blur()
-  }
+    if (e.code === "Enter") inputRef.current.blur();
+  };
 
   const finishEdit = () => {
-    onNewFile(value)
-    setEditing(false)
-  }
+    onNewFile(value);
+    setEditing(false);
+  };
 
   const startEditing = () => {
-    setEditing(true)
-    setValue('')
-  }
+    setEditing(true);
+    setValue("");
+  };
 
   return (
-    <button onClick={startEditing} 
-      className={`text-xs font-semibold px-1 block bg-gray-700 ${!editing && 'hover:bg-gray-500'} focus:outline-none border-t border-l border-r border-gray-600`}>
-      {
-        editing ? (
-          <div className="flex flex-row justify-center text-gray-300">
-            <input className="bg-transparent text-sm px-1 mx-1 focus:outline-none"
-              ref={inputRef}
-              autoFocus
-              spellCheck={false}
-              placeholder="Untitled"
-              onChange={updateValue}
-              onKeyPress={onKeyPress}
-              onBlur={finishEdit}
-              />
-          </div>
-        ) : (
-          <div className="flex flex-row justify-center text-gray-300 px-3 italic">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-            </svg>
-            <span>New</span>
-          </div>
-        )
-      }
+    <button
+      onClick={startEditing}
+      className={`text-xs font-semibold px-1 block bg-gray-700 ${
+        !editing && "hover:bg-gray-500"
+      } focus:outline-none border-t border-l border-r border-gray-600`}
+    >
+      {editing ? (
+        <div className="flex flex-row justify-center text-gray-300">
+          <input
+            className="bg-transparent text-sm px-1 mx-1 focus:outline-none"
+            ref={inputRef}
+            autoFocus
+            spellCheck={false}
+            placeholder="Untitled"
+            onChange={updateValue}
+            onKeyPress={onKeyPress}
+            onBlur={finishEdit}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-row justify-center text-gray-300 px-3 italic">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <span>New</span>
+        </div>
+      )}
     </button>
-  )
-}
+  );
+};
+
+const getFileMimeType = (filename) => {
+  const parts = filename.split(".");
+  const ext = parts[parts.length - 1];
+  if (ext.includes("js")) return "text/javascript";
+  if (ext.includes("css")) return "text/css";
+  if (ext.includes("html")) return "text/html";
+};
 
 const getFileLanguage = (filename) => {
-  const parts = filename.split(".")
-  const ext = parts[parts.length-1]
-  if (ext.includes("js")) return "javascript"
-  if (ext.includes("css")) return "css"
-  if (ext.includes("html")) return "html"
-}
+  const parts = filename.split(".");
+  const ext = parts[parts.length - 1];
+  if (ext.includes("js")) return "javascript";
+  if (ext.includes("css")) return "css";
+  if (ext.includes("html")) return "html";
+};
 
 function App() {
-  const iframeRef = useRef()
-  const editorRef = useRef()
-  const monacoRef = useRef()
+  const iframeRef = useRef();
+  const editorRef = useRef();
+  const monacoRef = useRef();
   // const monaco = useMonaco()
-  const [logs, setLogs] = useState([])
-  const [files, setFiles] = useState({})
-  const [iframeCode, setIframeCode] = useState()
-  const [resizing, setResizing] = useState(false)
+  const [logs, setLogs] = useState([]);
+  const [files, setFiles] = useState({});
+  const [iframeCode, setIframeCode] = useState();
+  const [resizing, setResizing] = useState(false);
 
   const [isEditorReady, setIsEditorReady] = useState(false);
   const [currentModel, setCurrentModel] = useState();
 
   const handleEditorDidMount = (editor, monaco) => {
-    console.log("editor mount")
     editorRef.current = editor;
     monacoRef.current = monaco;
-    const cachedFiles = JSON.parse(localStorage.getItem("jsplay-cache")) || defaultCached
-    let filesObj = {}
+    const cachedFiles =
+      JSON.parse(localStorage.getItem("jsplay-cache")) || defaultCached;
+    let filesObj = {};
     let startFile = null;
     Object.keys(cachedFiles).forEach((fileKey) => {
-      monacoRef.current.editor.createModel(cachedFiles[fileKey].value, getFileLanguage(cachedFiles[fileKey].name), fileKey);
-      filesObj[fileKey] = cachedFiles[fileKey]
+      monacoRef.current.editor.createModel(
+        cachedFiles[fileKey].value,
+        getFileLanguage(cachedFiles[fileKey].name),
+        fileKey
+      );
+      filesObj[fileKey] = cachedFiles[fileKey];
       if (startFile == null) {
         startFile = fileKey;
       }
-    })
+    });
 
-    setFiles(filesObj)
+    setFiles(filesObj);
 
     monacoRef.current.languages.typescript.javascriptDefaults.setEagerModelSync(
       true
     );
+    monacoRef.current.languages.typescript.javascriptDefaults.setCompilerOptions(
+      {
+        noLib: true,
+        allowNonTsExtensions: true,
+      }
+    );
+
     setIsEditorReady(true);
-    setCurrentModel(startFile)
-  }
+    setCurrentModel(startFile);
+  };
+
+  useLayoutEffect(() => {
+    const onMessage = (e) => {
+      if (e.data.event === "LOG") {
+        setLogs((l) => [{ method: e.data.level, data: e.data.args }, ...l]);
+      }
+    };
+    window.addEventListener("message", onMessage);
+
+    return () => {
+      window.removeEventListener("message", onMessage);
+    };
+  }, []);
 
   useEffect(() => {
     if (isEditorReady) {
@@ -196,60 +313,91 @@ function App() {
   }, [isEditorReady, currentModel]);
 
   const onNewFile = (filename) => {
-    const urn = `urn:${uuidv4()}`
-    const m = monacoRef.current.editor.createModel("// this is a dummy file", getFileLanguage(filename), urn);
+    const urn = `urn:${uuidv4()}`;
+    const m = monacoRef.current.editor.createModel(
+      "// this is a dummy file",
+      getFileLanguage(filename),
+      urn
+    );
+
     editorRef.current.setModel(m);
-    setFiles(f => {
-      let clone = {...f}
+    setFiles((f) => {
+      let clone = { ...f };
       clone[urn] = {
         value: "// this is a dummy file",
-        name: filename
-      }
-      return clone
-    })
-    setCurrentModel(urn)
-  }
+        name: filename,
+      };
+      return clone;
+    });
+    setCurrentModel(urn);
+  };
 
   const onChange = (value, event) => {
-    let clone = {...files}
-    clone[currentModel].value = value
-    localStorage.setItem('jsplay-cache', JSON.stringify(clone))
-  }
+    let clone = { ...files };
+    clone[currentModel].value = value;
+    localStorage.setItem("jsplay-cache", JSON.stringify(clone));
+  };
 
   const onRender = () => {
-    const value = monacoRef.current.editor.getModel(currentModel).getValue()
-    let valuesMap = {}
-    monacoRef.current.editor.getModels().forEach(m => {
+    const value = monacoRef.current.editor.getModel(currentModel).getValue();
+    let valuesMap = {};
+    monacoRef.current.editor.getModels().forEach((m) => {
       if (files[m.uri] != null) {
-        valuesMap[files[m.uri].name] = m.getValue()
+        valuesMap[files[m.uri].name] = m.getValue();
       }
-    })
-    setIframeCode(injectDependencies(value, valuesMap))
-  }
+    });
+    setLogs([]);
+    setIframeCode(injectDependenciesImportMap(value, valuesMap));
+    // setIframeCode(injectDependencies(value, valuesMap));
+  };
+  const setCurrentTab = (f) => {
+    // monacoRef.current.editor.getModels().forEach((m) => {
+    //   if (m.language === "javascript") {
+    //     monacoRef.current.languages.typescript.javascriptDefaults.addExtraLib(
+    //       m.getValue(),
+    //       m.uri
+    //     );
+    //   }
+    // });
+
+    setCurrentModel(f);
+  };
 
   return (
     <div className="h-screen w-screen flex">
-      <Sidebar/>
+      <Sidebar onRun={onRender} />
       <div className="flex flex-col w-full h-full">
-        <div className="p-1 max-h-8 h-8 bg-gray-800 border-b-2 border-gray-600 text-sm flex-grow flex justify-between">
+        {/* <div className="p-1 max-h-8 h-8 bg-gray-800 border-b-2 border-gray-600 text-sm flex-grow flex justify-between">
           <span></span>
-          <span className="text-gray-300 font-semibold">
-            Loading
-          </span>
-        </div>
-        <div class="flex-grow flex flex-row border-gray-600 border-l-2">
-          <SplitPane split="vertical"
+          <span className="text-gray-300 font-semibold">Loading</span>
+        </div> */}
+        <div class="flex-grow flex flex-row border-gray-600 border-l-2 max-h-screen">
+          <SplitPane
+            split="vertical"
             onResizeStart={() => setResizing(true)}
-            onResizeEnd={() => setResizing(false)}>
-              
-            <SplitPane split="horizontal">
+            onResizeEnd={(e) => {
+              console.log(e);
+              setResizing(false);
+            }}
+          >
+            <SplitPane split="horizontal" className="max-h-full">
               <Pane initialSize="90%">
                 <div className="flex flex-col h-full">
-                  <nav style={{ minHeight: '2.5rem' }} className="flex flex-col w-full overflow-x-scroll sm:flex-row h-10 px-4 pt-1 bg-gray-800">
-                    {
-                      Object.keys(files).map((f) => <Tab key={f} onToggle={f => setCurrentModel(f)} value={f} active={currentModel===f}>{files[f].name}</Tab>)
-                    }
-                    <NewFileTab onNewFile={onNewFile}/>
+                  <nav
+                    style={{ minHeight: "2.5rem" }}
+                    className="flex flex-col w-full overflow-x-scroll sm:flex-row h-10 px-4 pt-1 bg-gray-800"
+                  >
+                    {Object.keys(files).map((f) => (
+                      <Tab
+                        key={f}
+                        onToggle={setCurrentTab}
+                        value={f}
+                        active={currentModel === f}
+                      >
+                        {files[f].name}
+                      </Tab>
+                    ))}
+                    <NewFileTab onNewFile={onNewFile} />
                   </nav>
                   <Editor
                     theme="vs-dark"
@@ -260,30 +408,24 @@ function App() {
                     onMount={handleEditorDidMount}
                     onChange={onChange}
                   />
-                </div>   
+                </div>
               </Pane>
               <Pane initialSize="10%" minSize="10%" maxSize="40%">
                 <div class="px-3 overflow-scroll h-full bg-gray-800 border-t-2 border-gray-600 font-mono">
-                  <Console logs={logs} variant='dark'/>
+                  <Console logs={logs} variant="dark" />
                 </div>
               </Pane>
             </SplitPane>
             <Pane initialSize="50%" maxSize="60%" minSize="30%">
               <iframe
-                className={`w-full h-full border-gray-600 ${resizing ? 'pointer-events-none' : ''}`}
+                className={`w-full h-full border-gray-600 ${
+                  resizing ? "pointer-events-none" : ""
+                }`}
                 title="Code Preview"
                 srcDoc={iframeCode}
                 referrerPolicy="origin"
                 width="100%"
                 ref={iframeRef}
-                onLoad={() => {
-                  window.addEventListener('message', e => {
-                    const msg = e.data
-                    if (msg.level && msg.args) {
-                      // setLogs((currLogs) => [...currLogs, {method: msg.level, data: msg.args}])
-                    }
-                  })
-                }}
                 allowFullScreen
                 allow="midi; geolocation; microphone; camera; display-capture; encrypted-media;"
                 sandbox="allow-modals allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation allow-downloads"
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,59 +1,110 @@
-import React from 'react'
+import React from "react";
 
-export const Sidebar = () => {
+export const Sidebar = ({ onRun }) => {
   return (
     <aside className="flex flex-col items-center bg-gray-800 text-gray-200 shadow h-full">
-  
-        <div className="h-16 w-16 flex items-center ">
-          <a className="h-6 w-6 mx-auto text-green-400" href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-            </svg>
-          </a>
-        </div>
+      <div className="h-16 w-16 flex items-center ">
+        <a className="h-6 w-6 mx-auto text-green-400" href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+        </a>
+      </div>
 
-        <div className="h-16 flex items-center w-16 hover:bg-gray-600">
-          <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
+      <div className="h-16 flex items-center w-16 hover:bg-gray-600">
+        <button
+          className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none"
+          onClick={onRun}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
 
-        <div className="h-16 flex items-center w-16 hover:bg-gray-600">
-          <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z" clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
+      <div className="h-16 flex items-center w-16 hover:bg-gray-600">
+        <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
 
-        <div className="h-16 flex items-center w-16 hover:bg-gray-600">
-          <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-            </svg>
-          </button>
-        </div>
-        
-        <div className="h-16 flex items-center w-16 hover:bg-gray-600">
-          <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div>
-    
-        <div className="mt-auto h-16 flex items-center w-full">
-          <button
-            className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-700 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
-            </svg>
+      <div className="h-16 flex items-center w-16 hover:bg-gray-600">
+        <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+          </svg>
+        </button>
+      </div>
 
-          </button>
-        </div>
-    
-      </aside>
-  )
-}
+      <div className="h-16 flex items-center w-16 hover:bg-gray-600">
+        <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-600 hover:text-green-400 focus:outline-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div className="mt-auto h-16 flex items-center w-full">
+        <button className="h-16 mx-auto flex justify-center items-center w-full hover:bg-gray-700 focus:outline-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </aside>
+  );
+};
